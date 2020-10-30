bc71feec489da5e8019552df93b602d81fdc7a61
erre <jpwon@socar.kr>
2020-10-30 11:40:14 +0900

    Partial commit

--- transposition.py
+++ transposition.py
@@ -1,4 +1,18 @@
 
+class TranspositionTree:
+    def __init__(self, state):
+        self.state = state
+        self.score = None
+        self.play_again = False
+        self._actions = {}
+        self._future_score = {}
+
+    def next(self, action, next_state, play_again):
+        self._actions[action] = (next_state, play_again)
+
+    def negamax_score(self, depth, score):
+        self._future_score[depth] = score
+
 
 class TranspositionTable:
     def __init__(self):
@@ -10,5 +24,21 @@ class TranspositionTable:
         else:
             return None
 
-    def put(self, state, score, actions, depth):
-        self.table[state] = {'score': score, 'actions': actions, 'depth': depth}
+    def _prepare_node(self, state):
+        if state not in self.table:
+            self.table[state] = TranspositionTree(state)
+        return self.table[state]
+
+    def put_next(self, state, action, next_state, play_again):
+        node = self._prepare_node(state)
+        next_node = self._prepare_node(next_state)
+        node.next(action, next_state, play_again)
+
+    def put_score(self, state, score):
+        node = self._prepare_node(state)
+        node.score = score
+
+    def put_negamax_score(self, state, depth, score):
+        node = self._prepare_node(state)
+        node.depth_score(depth, score)
+
